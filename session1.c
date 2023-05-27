7640eab62315cf5006cc969d5f1c2b35ac0465d2
Paramesh <parameshdarshan1234@gmail.com>
2023-05-27 18:39:51 +0530

    page no 36 chapter 2

--- session1.c
+++ session1.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 void main()
 {
+    /*
+    char m;
     int s;
     printf("abc");
-    scanf("%d", &s);
-    printf("the given sumber is %d", s);
+    scanf("%d%c", &s, &m);
+    printf("the given number is %d\n", s);
+    printf(" the character is %c\n", m);
+    */
+    printf("learning the %c program ", 'c');
+    printf("i've just completed the chapter %d", 2);
+    printf("i'm %f sure ready to move on" 99.99);
+    printf("to the next chapter!\n");
 }
